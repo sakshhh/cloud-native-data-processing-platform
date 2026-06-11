8ff29358ea220430c13cbb3da3a8f4603238c2c1
Sakshi Pande <sakshi@Sakshis-MacBook-Air.local>
2026-06-11 18:57:34 +0530

    Add deployment pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,6 @@ resource "aws_security_group" "devops_sg" {
 
   ingress {
     description = "SSH"
-
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -41,12 +40,18 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+  ingress {
+  description = "FastAPI"
+  from_port   = 8000
+  to_port     = 8000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -62,9 +67,14 @@ resource "aws_instance" "devops_server" {
 
   key_name = aws_key_pair.devops_key.key_name
 
-    vpc_security_group_ids = [
-      aws_security_group.devops_sg.id
-    ]
+  vpc_security_group_ids = [
+    aws_security_group.devops_sg.id
+  ]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "devops-learning-server"
